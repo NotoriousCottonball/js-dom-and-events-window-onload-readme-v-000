65bfbb5d5444e2fa08064126579c97280c92eb97
NotoriousCottonball <gpianka1@gmail.com>
2019-04-27 21:27:14 -0700

    Append and Delayed Alert on Click with JQUERY

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-  $("#text").append("this is so freaking cool.");
+//$(document).ready(function() {
+//  $("#text").append("this is so freaking cool.");
+//});
+
+
+$(document).on("click", function() {
+    $("#text").append("this is so freaking cool.");
+    setTimeout(function() { alert("Delayed Click"); }, 1000);
 });
